3324f1367d8ec8d92a59e7b088e98d46f4875974
josvuduong <josvuduong@gmail.com>
2018-01-05 23:24:19 +0700

    [dev] edit ckfinder

--- app/Controller/Component/CommonComponent.php
+++ app/Controller/Component/CommonComponent.php
@@ -102,4 +102,93 @@ class CommonComponent extends Component
         );
         return $data;
     }
+    public function stripUnicode($str){
+    if(!$str) return false;
+    $unicode = array(
+        'a'=>'á|à|ả|ã|ạ|ă|ắ|ằ|ẳ|ẵ|ặ|â|ấ|ầ|ẩ|ẫ|ậ',
+        'A'=>'Á|À|Ả|Ã|Ạ|Ă|Ắ|Ằ|Ẳ|Ẵ|Ặ|Â|Ấ|Ầ|Ẩ|Ẫ|Ậ',
+        'd'=>'đ',
+        'D'=>'Đ',
+        'e'=>'é|è|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ',
+        'E'=>'É|È|Ẻ|Ẽ|Ẹ|Ê|Ế|Ề|Ể|Ễ|Ệ',
+        'i'=>'í|ì|ỉ|ĩ|ị',
+        'I'=>'Í|Ì|Ỉ|Ĩ|Ị',
+        'o'=>'ó|ò|ỏ|õ|ọ|ô|ố|ồ|ổ|ỗ|ộ|ơ|ớ|ờ|ở|ỡ|ợ',
+        'O'=>'Ó|Ò|Ỏ|Õ|Ọ|Ô|Ố|Ồ|Ổ|Ỗ|Ộ|Ơ|Ớ|Ờ|Ở|Ỡ|Ợ',
+        'u'=>'ú|ù|ủ|ũ|ụ|ư|ứ|ừ|ử|ữ|ự',
+        'U'=>'Ú|Ù|Ủ|Ũ|Ụ|Ư|Ứ|Ừ|Ử|Ữ|Ự',
+        'y'=>'ý|ỳ|ỷ|ỹ|ỵ',
+        'Y'=>'Ý|Ỳ|Ỷ|Ỹ|Ỵ'
+    );
+
+    foreach($unicode as $khongdau=>$codau) {
+        $arr=explode("|",$codau);
+        $str = str_replace($arr,$khongdau,$str);
+    }
+    return $str;
+    }
+    //Tạo slug
+    public function createSlug($str){
+        $str = trim($str);
+        if($str=="") return "";
+        $str = str_replace('"','',$str);
+        $str = str_replace("'",'',$str);
+        $str = str_replace("/",'',$str);
+        $str = stripUnicode($str);
+        $str = mb_convert_case($str,MB_CASE_LOWER,'utf-8');
+        // MB_CASE_Upper / MB_CASE_TITLE / MB_CASE_LOWER
+        $str = str_replace(' ','-',$str);
+        return $str;
+
+    }
+
+    //đệ quy category
+    public function cate_parent($data,$parent=0,$str="",$select=0){
+        foreach($data as $item){
+            $id = $item['id'];
+            $name = $item['name'];
+            if($item['parent_id'] == $parent){
+                if($select!=0 && $id == $select ){
+                    echo "<option value='$id' selected='selected'>$str $name</option>";
+                }else{
+                    echo "<option value='$id'>$str $name</option>";
+                }
+                cate_parent($data,$id,$str."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;",$select);
+            }
+        }
+    }
+?>
+<?php
+//Nhúng upload file
+function showUploadFile($idInput = '', $nameInput = '', $value = '', $number = '') { ?>
+    <script src="http://localhost/funnyfamily/public/include/ckfinder/ckfinder.js" ></script>
+    <script type="text/javascript">
+        function BrowseServerImg<?php echo $number; ?>() {
+            var finder = new CKFinder();
+            //finder.basePath = '../';
+            finder.selectActionFunction = SetFileFieldImg<?php echo $number; ?>;
+            finder.popup();
+        }
+
+        function SetFileFieldImg<?php echo $number; ?>(fileUrl) {
+            document.getElementById('<?php echo $idInput; ?>').value = fileUrl;
+        }
+    </script>
+    <input style="float: left;" type="text" name="<?php echo $nameInput; ?>" id='<?php echo $idInput; ?>' value="<?php echo $value; ?>" />
+    <input type="button" value="Lựa chọn" onclick="BrowseServerImg<?php echo $number; ?>();" />
+    <?php
 }
+?>
+<?php
+//Nhúng trình soạn thảo
+function showEditorInput($idEditor = '', $nameEditor = '', $content = '') { ?>
+    <script language="javascript" src="http://localhost/funnyfamily/public/include/ckeditor/ckeditor.js" type="text/javascript"></script>
+    <textarea style="border: 1px solid #abadb3;height: auto;" class="form-control"  id="<?php echo $idEditor; ?>" name="<?php echo $nameEditor; ?>" ><?php echo $content; ?></textarea>
+    <script type="text/javascript">CKEDITOR.replace('<?php echo $idEditor; ?>');</script>
+    <?php
+}
+?>
+
+<?php 
+} 
+?>
